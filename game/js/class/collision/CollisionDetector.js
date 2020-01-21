05665f7da245cd8ff3d73cbf527527ac310390c4
AngelRionCervi <a.rioncervi@gmail.com>
2020-01-21 14:06:50 +0100

    map collision detection

--- game/js/class/collision/CollisionDetector.js
+++ game/js/class/collision/CollisionDetector.js
@@ -3,6 +3,7 @@ export class CollisionDetector {
         this.player = player;
         this.map = map;
         this.missiles = missiles;
+        this.mapCollisionReduction = 5;
     }
 
     mapPlayerCollision() {
@@ -14,45 +15,41 @@ export class CollisionDetector {
         let playerY = playerPos.y;
         let baseSizeX = playerSpecs.baseSizeX;
         let baseSizeY = playerSpecs.baseSizeY;
+        let collReduction = this.mapCollisionReduction;
         let blockSize = this.map.blockSize;
 
         let isColl = false;
 
         this.map.coords.forEach((v) => {
-            // first check if player is inside first pixels of square
-            if (playerX + baseSizeX/2 >= v.x
-                && playerX - baseSizeX/2 <= v.x + blockSize 
-                && playerY + baseSizeY/2 >= v.y 
-                && playerY - baseSizeY/2 <= v.y + blockSize) {
-                
-                //then checks for the sides 
-               
-                // x
-                if (playerY - baseSizeY/2 > v.y && playerY - baseSizeY/2 < v.y + blockSize) {
-                    if (playerX + baseSizeX/2 <= v.x) {
+            
+                //x
+                if ((playerY + baseSizeY/2 - collReduction > v.y && playerY + baseSizeY/2 - collReduction < v.y + blockSize) 
+                || (playerY - baseSizeY/2 + collReduction > v.y && playerY - baseSizeY/2 + collReduction < v.y + blockSize)) {
+
+                    if (playerX - baseSizeY/2 <= v.x && playerX + baseSizeY/2 >= v.x) {
                         console.log("left coll")
                         isColl = 'left';
 
-                    } else if (playerX + baseSizeX/2 >= v.x + blockSize) {
+                    } else if (playerX - baseSizeY/2 <= v.x + blockSize && playerX + baseSizeY/2 >= v.x) {
                         console.log("right coll")
                         isColl = 'right';
 
                     }
-                }
-               
-                // y
-                if (playerX - baseSizeX/2 > v.x && playerX - baseSizeX/2 < v.x + blockSize) {
-                    if (playerY + baseSizeY/2 <= v.y) {
+                //y
+                } else if ((playerX + baseSizeY/2 - collReduction > v.x && playerX + baseSizeY/2 - collReduction < v.x + blockSize) 
+                || (playerX - baseSizeY/2 + collReduction > v.x && playerX - baseSizeY/2 + collReduction < v.x + blockSize)) {
+
+                    if (playerY - baseSizeY/2 <= v.y && playerY + baseSizeY/2 >= v.y) {
                         console.log("top coll")
                         isColl = 'top';
 
-                    } else if (playerY + baseSizeY/2 >= v.y + blockSize) {
+                    } else if (playerY - baseSizeY/2 <= v.y + blockSize && playerY + baseSizeY/2 >= v.y) {
                         console.log("bottom coll")
                         isColl = 'bottom';
 
                     }
                 }
-            } 
+            
         })
 
         return isColl;
